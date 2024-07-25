965954805ec38945412b0756a7b256db5e88bb17
kimyeonji713 <hfgj0014@naver.com>
2024-07-25 18:20:41 +0900

    비구조할당

---
+++ src/components/1. prop/IntroEx.js
@@ -0,0 +1,15 @@
+export const IntroEx = ({ name, age }) => {
+  //     console.log(props);
+  //   const name = props.name;
+  //   const age = props.age;
+
+  //   const { name, age } = props;
+  //   console.log(name, age);
+
+  return (
+    <div>
+      <h2>이름: {name}</h2>
+      <h2>나이: {age}</h2>
+    </div>
+  );
+};
